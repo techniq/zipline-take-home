c589c9d31d48b4dda70dd293eb11129b9451069f
Sean Lynch <techniq35@gmail.com>
2024-10-20 18:29:28 -0400

    Setup basic app structure with dependencies (LayerChart, Svelte UX, and LayerStack)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,46 @@
 <script lang="ts">
+	import { AppBar, AppLayout, NavItem, settings, ThemeSelect } from 'svelte-ux';
 	import '../app.css';
+	import { page } from '$app/stores';
+	import { mdiHome } from '@mdi/js';
+
 	let { children } = $props();
+
+	settings({
+		components: {
+			AppLayout: {
+				classes: {
+					aside: 'border-r',
+					nav: 'bg-surface-300 py-2'
+				}
+			},
+			AppBar: {
+				classes:
+					'bg-primary text-primary-content shadow-md [text-shadow:1px_1px_2px_theme(colors.primary-700)]'
+			},
+			NavItem: {
+				classes: {
+					root: 'text-sm text-surface-content/70 pl-6 py-2 hover:bg-surface-100/70 relative',
+					active:
+						'text-primary bg-surface-100 font-medium before:absolute before:bg-primary before:rounded-full before:w-1 before:h-2/3 before:left-[6px] shadow z-10'
+				}
+			}
+		}
+	});
 </script>
 
-{@render children()}
+<AppLayout>
+	<svelte:fragment slot="nav">
+		<NavItem text="Home" icon={mdiHome} path="/" currentUrl={$page.url} />
+	</svelte:fragment>
+
+	<AppBar title="Example">
+		<div slot="actions">
+			<ThemeSelect />
+		</div>
+	</AppBar>
+
+	<main>
+		{@render children()}
+	</main>
+</AppLayout>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,43 @@
 import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
+import layerstack from '@layerstack/tailwind/plugin';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		'./node_modules/svelte-ux/**/*.{svelte,js}',
+		'./node_modules/layerchart/**/*.{svelte,js}'
+	],
 
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	// See customization docs: https://svelte-ux.techniq.dev/customization
+	ux: {
+		themes: {
+			light: {
+				primary: colors['violet']['500'],
+				'primary-content': 'white',
+				secondary: colors['cyan']['500'],
+				'surface-100': 'white',
+				'surface-200': colors['gray']['100'],
+				'surface-300': colors['gray']['300'],
+				'surface-content': colors['gray']['900'],
+				'color-scheme': 'light'
+			},
+			dark: {
+				primary: colors['violet']['500'],
+				'primary-content': 'white',
+				secondary: colors['cyan']['500'],
+				'surface-100': colors['zinc']['800'],
+				'surface-200': colors['zinc']['900'],
+				'surface-300': colors['zinc']['950'],
+				'surface-content': colors['zinc']['100'],
+				'color-scheme': 'dark'
+			}
+		}
+	},
+
+	plugins: [layerstack]
 } as Config;
